20d439910531fbed867c7d01cfc21f0c12c92cab
unknown <ayushniroula644@gmail.com>
2024-05-29 16:38:53 +0545

    css

--- CLIENT/src/component/Navbar/Navbar.tsx
+++ CLIENT/src/component/Navbar/Navbar.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Navbar() {
   return (
   <>
-  <div className="nav-bar">
-   <div className="logo"></div>
+  <div className="nav-bar flex h-[100px] w[100%] bg-orange-500">
+   <div className="logo">Logo</div>
    <div className="search-box">
    <input type="text" placeholder='Search here..' />
    </div>
