c2b673da8307c097cafa0cd173daac0e2461ebe3
unknown <ayushniroula644@gmail.com>
2024-05-29 16:25:49 +0545

    Nav

---
+++ CLIENT/src/component/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+  <>
+  <div className="nav-bar">
+   <div className="logo"></div>
+   <div className="search-box">
+   <input type="text" placeholder='Search here..' />
+   </div>
+ 
+
+
+
+  </div>
+  
+  
+  
+  </>
+  )
+}
+
+export default Navbar
