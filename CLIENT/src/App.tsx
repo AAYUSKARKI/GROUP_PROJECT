5c536005ac45b44587bf50597266d83788070843
AAYUS KARKI <alexkarki2060@gmail.com>
2024-05-29 18:42:34 +0545

    Cards added

--- CLIENT/src/App.tsx
+++ CLIENT/src/App.tsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './component/Home/Home';
+import Home from './components/Home/Home';
 function App() {
   return (
     <BrowserRouter>
---
+++ CLIENT/src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar/Navbar"
+import Cards from '@/components/Cards/Cards'
+function Home() {
+  return (
+   <>
+   <Navbar/>
+   <Cards/>
+   </>
+  )
+}
+
+export default Home
