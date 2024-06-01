30fe48cf0e256fe31bc830f79949840a19a88a8d
Sandesh laksamba <sandeshlaksamba123@gmail.com>
2024-06-01 12:10:24 +0545

    signup login

--- CLIENT/src/pages/Login.tsx
+++ CLIENT/src/pages/Login.tsx
@@ -1,38 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleOnChange = (e) =>{
+    const {name, value} = e.target
+       setData((prev)=>{
+      return{
+        ...prev,
+        [name] : value
+      }
+    })
+  }
+const handlesubmit = (e)=>{
+  e.preventDefault()
+}
+  
+
+  console.log("user data", data)
   return (
-    <section id="login " >
+    <section id="login ">
       <div className=" mx-auto container p-4 ">
         <div className=" text-black bg-white p-8 m-3 w-full max-w-[500px] mx-auto rounded-xl">
           <div className="w-20 h-20 mx-auto ">
             <FaUserCircle className="text-6xl" />
           </div>
-           <h2 className="text-xl w-96 mx-auto">Welcome to lucid merch ! please login</h2>
-          <form className="  flex flex-col gap-4 justify-center items-center p-4 rounded">
-           
+          <h2 className="text-xl w-96 mx-auto">
+            Welcome to lucid merch ! please login
+          </h2>
+          <form onSubmit={handlesubmit} className="  flex flex-col gap-4 justify-center items-center p-4 rounded">
             <div className=" flex flex-col w-full gap-2 ">
               <label className="font-bold">Email:</label>
               <input
-                type="text"
+                type="email"
+                name="email"
+                value={data.email}
+                onChange={handleOnChange}
                 className="bg-slate-300 rounded w-full h-10 text-black p-2"
                 placeholder="Enter your email"
+                required
               />
             </div>
             <div className=" flex flex-col w-full  gap-2">
               <label className="font-bold">Password:</label>
-              <input
-                type="password"
-                className="bg-slate-300 rounded w-full h-10 text-black p-2"
-                placeholder="Enter your password "
-              />
+              <div className="flex justify-center items-center">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  onChange={handleOnChange}
+                  name="password"
+                  value={data.password}
+                  placeholder="Enter your password "
+                  required
+                  className="bg-slate-300 rounded w-full h-10 text-black p-2"
+                />
+                <div
+                  className="cursor-pointer text-xl -ml-8 "
+                  onClick={() => setShowPassword((preve) => !preve)}
+                >
+                  <span>{showPassword ? <FaEye /> : <FaEyeSlash />}</span>
+                </div>
+              </div>
             </div>
-            <p className='block w-fit ml-auto hover:underline hover:text-blue-600 text-blue-600 font-bold'>forgot password ?</p>
+            <p className="block w-fit ml-auto hover:underline hover:text-blue-600 text-blue-600 font-bold">
+              forgot password ?
+            </p>
 
-            <button className="bg-blue-600 hover:bg-blue-500 transition-all h-10 w-full rounded-full font-bold mt-7">Login </button>
+            <button className="bg-blue-600 hover:bg-blue-500 transition-all h-10 w-full rounded-full font-bold mt-7">
+              Login
+            </button>
 
-            <p>Don't have Account ? <Link to={"/Sign-up"} className=" hover:underline text-blue-600 font-bold">Register Here</Link></p>
+            <p>
+              Don't have Account ?{" "}
+              <Link
+                to={"/Sign-up"}
+                className=" hover:underline text-blue-600 font-bold"
+              >
+                Register Here
+              </Link>
+            </p>
           </form>
         </div>
       </div>
--- CLIENT/src/pages/SignUp.tsx
+++ CLIENT/src/pages/SignUp.tsx
@@ -1,12 +1,69 @@
-import React from 'react'
+import imageTobase64 from "@/helpers/imageTobase";
+import React, { useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash, FaGoogle } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [confirmedPassword, setConfirmedPassword] = useState(false);
+
+  const [data, setData] = useState({
+    name:"", 
+    email: "",
+    password:"",
+    confirmPassword: "",
+    avatar: ""
+  })
+
+  const handleOnChange = (e)=>{
+    const {name, value} = e.target
+    setData((prev)=>{
+     return{
+      ...prev,
+      [name] : value
+     }
+    })
+  }
+
+  const handleUploadPic = async(e) =>{
+    const file = e.target.files[0]
+  
+    
+    const imagePic = await imageTobase64(file)
+    
+    setData((prev)=>{
+      return{
+        ...prev,
+        avatar : imagePic
+      }
+    })
+
+  }
   return (
-    <section id='sign-up'>
-      <div className='container  mx-auto  p-7'>
-        <div className='text-black bg-white p-8 m-3 w-full max-w-[500px] mx-auto rounded-xl'>
-            <h1>sign up </h1>
+    <section id="sign-up">
+      <div className="container  mx-auto  p-7">
+        <div className="text-black bg-white p-8 m-3 w-full max-w-[500px] mx-auto rounded-xl">
+          <div className="w-20 h-20 mx-auto relative overflow-hidden rounded-full">
+            <div>
+              <img src ={data.avatar } alt="login icons" />
+            </div>
             <form>
+              <label>
+                <div className="text-xs bg-opacity-80 bg-slate-200 pb-4 pt-2 cursor-pointer text-center absolute bottom-0 w-full">
+                  Upload Photo
+                </div>
+                <input
+                  type="file"
+                  className="hidden"
+                  onChange={handleUploadPic}
+                />
+              </label>
+            </form>
+          </div>
+
+          <form className=" flex flex-col gap-4">
             <div className=" flex flex-col w-full gap-2 ">
               <label className="font-bold">FULL NAME:</label>
               <input
@@ -19,7 +76,7 @@ function SignUp() {
             <div className=" flex flex-col w-full gap-2 ">
               <label className="font-bold">EMAIL:</label>
               <input
-                type="text"
+                type="email"
                 className="bg-slate-300 rounded w-full h-10 text-black p-2"
                 placeholder="Enter your email"
               />
@@ -27,34 +84,60 @@ function SignUp() {
 
             <div className=" flex flex-col w-full gap-2 ">
               <label className="font-bold">PASSWORD:</label>
-              <input
-                type="text"
-                className="bg-slate-300 rounded w-full h-10 text-black p-2"
-                placeholder="Enter password "
-                required
-              />
+              <div className="flex justify-center items-center">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className="bg-slate-300 rounded w-full h-10 text-black p-2"
+                  placeholder="Enter password "
+                  required
+                />
+                <div
+                  className="cursor-pointer text-xl -ml-8 "
+                  onClick={() => setShowPassword((preve) => !preve)}
+                >
+                  <span>{showPassword ? <FaEye /> : <FaEyeSlash />}</span>
+                </div>
+              </div>
             </div>
 
             <div className=" flex flex-col w-full gap-2 ">
               <label className="font-bold">CONFIRM PASSWORD</label>
-              <input
-                type="text"
-                className="bg-slate-300 rounded w-full h-10 text-black p-2"
-                placeholder="Conform password"
-                required
-              />
+              <div className="flex justify-center items-center">
+                <input
+                  type={confirmedPassword ? "text" : "password"}
+                  className="bg-slate-300 rounded w-full h-10 text-black p-2"
+                  placeholder="Conform password"
+                  required
+                />
+                <div
+                  className="cursor-pointer text-xl -ml-8 "
+                  onClick={() => setConfirmedPassword((preve) => !preve)}
+                >
+                  <span>{confirmedPassword ? <FaEye /> : <FaEyeSlash />}</span>
+                </div>
+              </div>
             </div>
-            <button className="bg-blue-600 hover:bg-blue-500 transition-all h-10 w-full rounded-full font-bold mt-7">sign up </button>
-           
-            </form>
-            <div className='w-ful h-4 p-5'>
-               <button>google Account</button>
-            </div>
+            <Link
+              to={"/Login"}
+              className="bg-blue-600 text-center py-2 hover:bg-blue-500 transition-all h-10 w-full rounded-full font-bold mt-7"
+            >
+              sign up
+            </Link>
+          </form>
+
+          {/* -------google sign up ------*/}
+
+          <div className="relative flex items-center justify-center w-full mt-4 border-t">
+            <span className="absolute px-2 bg-white text-gray-600">or</span>
+          </div>
+          <button className="flex items-center justify-center bg-orange-400 w-full px-4 py-4 mt-4  font-semibold text-gray-700 border rounded-md ">
+            <FaGoogle className="text-red-700 text-2xl mx-2" />
+            <span>Sign Up with Google</span>
+          </button>
         </div>
       </div>
-
     </section>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
